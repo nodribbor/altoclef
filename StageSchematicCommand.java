110e8f18daa9bf1164188d50ce9a75cd29a9fe5e
nodribbor <nod.ribbor.08@icloud.com>
2026-02-19 00:35:20 +0100

    Update constructor for StageSchematicCommand to fix signature for Minecraft 1.21+

---
+++ StageSchematicCommand.java
@@ -0,0 +1,8 @@
+// Update of StageSchematicCommand.java
+
+public class StageSchematicCommand extends Command {
+    public StageSchematicCommand() {
+        super(List.of("stageSchematic", "litematicaStage", "schematicStage"), "Stage all required resources for the currently selected Litematica schematic placement into nearby chests");
+    }
+    // Other methods and logic
+}
